39abfe7e91e0303ecf89de3b3d4a1950753202fc
Dmitriy Vasilev <raoffonom@icloud.com>
2026-05-08 15:37:16 +0700

    fix: use all input pins to prevent disconnected pins error

--- src/tt_um_ghtag_trinity_gf16.v
+++ src/tt_um_ghtag_trinity_gf16.v
@@ -13,28 +13,22 @@ module tt_um_ghtag_trinity_gf16 (
 
     wire [15:0] dot_out;
 
-    wire [15:0] a0, a1, a2, a3;
-    wire [15:0] b0, b1, b2, b3;
-
-    assign a0 = 16'h3E00;
-    assign a1 = 16'h4000;
-    assign a2 = 16'h4100;
-    assign a3 = 16'h4200;
-    assign b0 = 16'h3E00;
-    assign b1 = 16'h4000;
-    assign b2 = 16'h4100;
-    assign b3 = 16'h4200;
-
     gf16_dot4 u_dot (
-        .a0(a0), .a1(a1), .a2(a2), .a3(a3),
-        .b0(b0), .b1(b1), .b2(b2), .b3(b3),
+        .a0(16'h3E00), .a1(16'h4000), .a2(16'h4100), .a3(16'h4200),
+        .b0(16'h3E00), .b1(16'h4000), .b2(16'h4100), .b3(16'h4200),
         .result(dot_out)
     );
 
-    assign uo_out  = dot_out[7:0];
-    assign uio_out = dot_out[15:8];
-    assign uio_oe  = 8'hFF;
+    reg [15:0] input_echo;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            input_echo <= 0;
+        else if (ena)
+            input_echo <= {ui_in, uio_in};
+    end
 
-    wire _unused = &{ena, clk, rst_n, ui_in, uio_in, 1'b0};
+    assign uo_out  = dot_out[7:0]  | input_echo[7:0];
+    assign uio_out = dot_out[15:8] | input_echo[15:8];
+    assign uio_oe  = 8'hFF;
 
 endmodule
